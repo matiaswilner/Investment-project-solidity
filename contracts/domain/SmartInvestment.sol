1a60430ca9eb63e6b3ebe53c8baf6b022cd5428d
Alfonso <alfoirazabal@live.com>
2021-11-05 19:43:08 -0300

    Requerimientos propuestas 2, 4, 6. Requerimiento roles 6

--- contracts/domain/SmartInvestment.sol
+++ contracts/domain/SmartInvestment.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.9;
 
 contract SmartInvestment {
 
+    enum SystemState { Closed, Proposal, Voting }
+    SystemState public systemState;
+
+    uint256 proposalIdsCounter = 1;
     uint256 ownerIdsCounter = 1;
     uint256 makerIdsCounter = 1;
     uint256 auditorIdsCounter = 1;
     uint256 voterIdsCounter = 1;
 
+    mapping(uint256 => Proposal) public proposals;
     mapping(address => Owner) public owners;
     mapping(address => Maker) public makers;
     mapping(address => Auditor) public auditors;
@@ -30,6 +35,18 @@ contract SmartInvestment {
         uint256 id;
     }
 
+    /*
+        REQUERIMIENTO PROPUESTAS 6
+    */
+    struct Proposal {
+        uint256 id;
+        bool isOpen;
+        string name;
+        string description;
+        uint256 minimumInvestment;
+        uint256 maker;
+    }
+
     modifier isOwner() {
         require(owners[msg.sender].id != 0);
         _;
@@ -38,15 +55,54 @@ contract SmartInvestment {
     constructor() {
         owners[msg.sender] = Owner(ownerIdsCounter);
         ownerIdsCounter++;
+        systemState = SystemState.Closed;
     }
 
     /*
+        REQUERIMIENTO ROLES 5
         Solo un owner puede agregar otro owner, entonces
         debemos usar el modifier isOwner.
     */
     function addOwner(address newOwnerAddress) public isOwner {
         owners[newOwnerAddress] = Owner(ownerIdsCounter);
         ownerIdsCounter++;
+    }
+
+    /*
+        REQUERIMIENTO ROLES 6
+        Solo los owners podrán registrar makers, entonces
+        debemos usar el modifier isOwner
+    */
+    function addMaker(address newMakerAddress) public isOwner {
+        makers[newMakerAddress] = Maker(makerIdsCounter);
+        makerIdsCounter++;
+    }
+
+    /*
+        REQUERIMIENTO PROPUESTAS 2, 4
+    */
+    function setStateClosed() public isOwner {
+        if (systemState == SystemState.Proposal) {
+            uint256 lastProposalId = proposalIdsCounter - 1;
+            uint256 lastProposalId2 = lastProposalId - 1;
+            if (lastProposalId > 0 && lastProposalId2 > 0) {
+                Proposal memory proposal1 = proposals[lastProposalId];
+                Proposal memory proposal2 = proposals[lastProposalId2];
+                if (proposal1.isOpen && proposal2.isOpen) {
+                    // 🤔 Hay q cerrar las dos propuestas??
+                    systemState = SystemState.Closed;
+                }
+                else {
+                    assert(false);
+                }
+            }
+            else {
+                assert(false);
+            }
+        }
+        else {
+            assert(false);
+        }
     }
 
 }
